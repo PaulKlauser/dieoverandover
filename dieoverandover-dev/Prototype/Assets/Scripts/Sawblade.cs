bcc2c6fd799e31cec9a5451c25ae64c4f618e03c
muppetman <paul.klauser@gmail.com>
2014-01-25 18:38:47 -0500

    Bloody saw blades included

--- dieoverandover-dev/Prototype/Assets/Scripts/Sawblade.cs
+++ dieoverandover-dev/Prototype/Assets/Scripts/Sawblade.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class Sawblade : Obstacle {
 	
 	public float speed = 300;
+	Material bloody;
+
+	void Start() {
+		renderer.enabled = false;
+	}
 
 	void Update () {
-		transform.Rotate(Vector3.forward * speed * Time.deltaTime,Space.World);
+		transform.Rotate(Vector3.forward * -speed * Time.deltaTime,Space.World);
 	}
 	
 	
 	void OnTriggerEnter(Collider c) {
-		ShowTrueColor ();
+		//ShowTrueColor ();
+
+		//renderer.material = bloody;
+		renderer.enabled = true;
 		if (c.tag == "Player") {
 			c.GetComponent<Entity>().TakeDamage(10);
 		}
