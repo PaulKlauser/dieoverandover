239f77b07ef49893c3a72fc14643874a7219b27b
muppetman <paul.klauser@gmail.com>
2014-01-25 01:26:03 -0500

    Saturday Morning.

---
+++ New Unity Project 1/Assets/Scripts/Ceiling.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ceiling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector2 (0, 8);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project 1/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float playerSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		//Player spawn point
+
+		//Where the player will start when the game is played
+
+		transform.position = new Vector2 (0, 3);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//player to move left/right, with arrow keys
+
+		transform.Translate (Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime);
+		transform.Translate (Vector3.up * Input.GetAxis ("Jump"));
+	
+	}
+}
